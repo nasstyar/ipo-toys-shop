648f44dac7cf0085a3cfacf950eccfbe16dbdbe4
nastya <anastasiakoz019@gmail.com>
2026-06-25 07:10:26 +0300

    23

--- store/views.py
+++ store/views.py
@@ -249,7 +249,8 @@ def checkout(request):
 from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from .serializers import OrderSerializer, (
+from .serializers import (
+    OrderSerializer,
     CategorySerializer, ManufacturerSerializer, ProductSerializer,
     CartSerializer, CartItemSerializer
 )
