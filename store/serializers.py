1617b9d270348a25aafcc60bb13285fd546673b0
nastya <anastasiakoz019@gmail.com>
2026-06-25 07:14:47 +0300

    23

--- store/serializers.py
+++ store/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Category, Manufacturer, Product, Cart, CartItem
+from .models import Category, Manufacturer, Product, Cart, CartItem, Order, OrderItem
 
 
 class CategorySerializer(serializers.ModelSerializer):
